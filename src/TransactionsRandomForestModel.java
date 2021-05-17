1f1f6d123dc8057ce2282b962c640b89651a5238
Nagendra Nainala <nnagendra0446@gmail.com>
2021-05-17 09:54:45 -0700

    Improved comments

--- src/TransactionsRandomForestModel.java
+++ src/TransactionsRandomForestModel.java
@@ -15,9 +15,9 @@ public class TransactionsRandomForestModel {
 	private static Classifier rf_classifier;
 	private static Instances training_instances;
 	
-	public static void trainModel(String train_data_path, String model_file_name) throws Exception {
+	public static void trainAndEvaluateModel(String train_data_path, String model_file_name) throws Exception {
 		
-		// Creating Training data instances.
+		// STEP 1: Creating Training data instances.
 		File inputFile = new File(train_data_path);
 		CSVLoader csv_loader = new CSVLoader();   
 		try {
@@ -26,31 +26,26 @@ public class TransactionsRandomForestModel {
 		} catch (IOException e) {
 				System.out.println("Error loading the dataset.");
 				e.printStackTrace();
-		}
+			}
 		training_instances.setClassIndex((training_instances.numAttributes() - 1));
 
-		// Constructing Model
+		// STEP 2: Constructing and building Model
 		rf_classifier = new RandomForest();
-
-		// Building the Model
 		rf_classifier.buildClassifier(training_instances);
 
-		// Saving the model in the saved_models directory
-		weka.core.SerializationHelper.write("saved_models_objects\\" + model_file_name, rf_classifier);
-		weka.core.SerializationHelper.write("saved_models_objects\\training_instances_class_attribute.obj", training_instances.classAttribute());
-	}
-	
-	public static void printStats() throws Exception {
-		
-		// Evaluating using k-fold cross-validation
+		// STEP 3: Saving the model in the saved_models directory
+		weka.core.SerializationHelper.write("saved_models\\" + model_file_name, rf_classifier);
+		weka.core.SerializationHelper.write("saved_models\\training_instances_class_attribute.obj", training_instances.classAttribute());
+		System.out.println("Model Trained and Saved to \"saved_models\" Successfully.");
+
+		// STEP 4: Evaluating using k-fold cross-validation
 		Evaluation evaluation = new Evaluation(training_instances);
 		evaluation.crossValidateModel(rf_classifier, training_instances, 10, new Random(1));
 
-		// Displaying the results  
+		// STEP 5: Displaying the results  
 		System.out.println(rf_classifier);
 		System.out.println(evaluation.toSummaryString("\nResults\n======\n", true));
 		System.out.println(evaluation.toClassDetailsString());
 		System.out.println(evaluation.toMatrixString());
 	}
 }
-
